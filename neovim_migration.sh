854c608eb290753ae7177144207906c82d681623
Mikel Iturbe <mikel@hamahiru.org>
2023-08-31 05:38:34 +0200

    Update nvim migration script

--- neovim_migration.sh
+++ neovim_migration.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-# Script to migrate to Neovim from Vim
-
-# Install neovim
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install neovim
-
 # Migrate dotfiles from Vim
 mkdir ~/.config/nvim
-ln -s ./vim/.vimrc ~/.config/nvim/init.vim
+stow -t ~/.config/nvim vim
+ln -s ~/.config/nvim/.vimrc ~/.config/nvim/init.vim
 
-# Support for Python extensions
-sudo apt-get install python-dev python-pip python3-dev python3-pip
+nvim +PlugInstall
 
-sudo pip2 install --upgrade neovim
-sudo pip3 install --upgrade neovim
